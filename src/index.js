2df16eaded4c02507711ad1bab87c962b5ad490a
QuanNguyen <quannguyen151197@hmail.com>
2023-10-02 18:33:47 +0700

    added husky and lint-stagged

--- src/index.js
+++ src/index.js
@@ -29,13 +29,11 @@ app.engine(
     }),
 );
 app.set('view engine', 'hbs');
-app.set('views',        path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources/views'));
 
 // Routes init
 route(app);
 
-app.listen(port, () => 
-{
-  
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
